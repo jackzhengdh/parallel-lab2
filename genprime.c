11deaf87dc36092f7896eb5ca377274bb4aecce4
jackzhengdh <jackzhengdh@gmail.com>
2017-04-15 22:37:54 -0400

    finished genprime

---
+++ genprime.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <omp.h>
+
+// serial version of SoE algorithm
+
+int main(int argc, char* argv[]) {
+
+	int thread_count;
+
+	int N = atoi(argv[1]);
+	thread_count = atoi(argv[2]);
+	int *isPrime = malloc(N*sizeof(int));
+	int *primes = malloc(N*sizeof(int));
+	memset(isPrime, 1, N*sizeof(int));
+	memset(primes, 0, N*sizeof(int));
+
+	int nPrime = 0;
+	int i;
+
+	// time prime-finding algorithm
+	double tstart = 0.0, ttaken;
+	tstart = omp_get_wtime();
+
+	#pragma omp parallel for num_threads(thread_count)
+	for (i = 2; i < N; i++) {
+		if (isPrime[i]) {
+			int j;
+			for (j = i + i; j < N; j += i)
+				isPrime[j] = 0;
+		}
+	}
+
+	int cnt = 0;
+	for (i = 2; i < N; i++) {
+		if (isPrime[i]) {
+			primes[nPrime++] = i;
+			cnt++;
+		}
+	}
+	ttaken = omp_get_wtime() - tstart;
+
+	printf("Pi(%d) = %d\n", N, cnt);
+	printf("Time taken = %f\n", ttaken);
+
+	FILE* fp;
+	char filename[10];
+	sprintf(filename, "%d.txt", N);
+	fp = fopen(filename, "w+");
+	
+	i = 1;
+	int pre = primes[0];
+	fprintf(fp, "%d, %d, %d\n", i, pre, 0);
+	while(1) {
+		if (!primes[i])
+			break;
+		fprintf(fp, "%d, %d, %d\n", i + 1, primes[i], primes[i] - pre);
+		pre = primes[i++];
+	}
+	fclose(fp);
+	return 0;
+}
